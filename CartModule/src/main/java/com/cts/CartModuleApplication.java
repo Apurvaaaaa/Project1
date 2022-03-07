25ff3df042b389314ece68b0e32e996a5c81df5c
Apurva Narkhede <Apurva Narkhede@LAPTOP-OV4RFG58>
2022-03-07 14:40:46 +0530

    done

---
+++ CartModule/src/main/java/com/cts/CartModuleApplication.java
@@ -0,0 +1,50 @@
+package com.cts;
+
+import java.net.InetAddress;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+// Cart Microservice
+ 
+@SpringBootApplication
+@EnableEurekaClient
+@Slf4j
+public class CartModuleApplication {
+
+	public static void main(String[] args) {
+		log.info("Main started...");
+		SpringApplication.run(CartModuleApplication.class, args);
+		log.info("Main ended...");
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplateBean() {
+		return new RestTemplate();
+	}	
+	@Bean
+	@Autowired
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		String ip = null;
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (Exception e) {
+		}
+		config.setNonSecurePort(8002);
+		config.setIpAddress(ip);
+		config.setPreferIpAddress(true);
+		return config;
+	}
+	
+}
